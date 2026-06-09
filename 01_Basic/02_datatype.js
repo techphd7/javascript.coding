490435df444cde208b68b501259ad8667459ccd1
techphd7 <techphd7@gmail.com>
2026-06-09 11:00:06 +0000

    updated datatyped added non primitive

--- 01_Basic/02_datatype.js
+++ 01_Basic/02_datatype.js
@@ -3,6 +3,9 @@
 //alert(3+3)//because this is use in browser not in node.js
 
 console.log("sum :",3+3);
+//data types in js
+//1.Primitives 2.Non-Primitives
+//1.Primitives->string,number,bigint,boolean,undefined,null,symbol
 
 
 let name="rakesh";//string
@@ -19,6 +22,29 @@ console.log([name,age,accountNO,ifrunning,average,find,box,]);
  console.log("rakesh type :" ,typeof name);//string
  console.log(typeof null);//object
  console.log(typeof undefined);//undefined
+
+
+ //2.Non-Primitives->object,array,function
+
+ let cricketer={
+    name:"virat",
+    age:35,
+    country:"india",
+    isplaying:true
+ }
+ console.log("object");
+ console.log(cricketer);//object
+
+ let arr=[1,2,3,4,5];
+ 
+ console.log("array");//object
+ console.log(arr);
+
+ function add(a,b){
+    return a+b;
+ }
+ console.log("function");
+ console.log(add(5,3));
  
  
  
