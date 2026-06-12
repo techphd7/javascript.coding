5e1ead3ca3e439c56b20275470a641a0af2c78c4
techphd7 <techphd7@gmail.com>
2026-06-12 13:20:10 +0000

    function s

--- 02_Basic/05_Functions.js
+++ 02_Basic/05_Functions.js
@@ -23,3 +23,30 @@ return `Welcome back, ${username}!`
 //const message=loginUserMessage("Prabhanshu");
 const message=loginUserMessage();//undefined because we are not passing any argument to the function, so username is undefined
 console.log(message);
+
+function calculateCartPrice(value1,value2,...num1){
+return num1; 
+}
+
+console.log(calculateCartPrice(200,400,500,700,800,900));//value1=200,value2=400and rest is array of num1
+
+const user={
+    username:"prabhanshu",
+    price:199
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price${anyObject.price}`);
+    
+}
+handleObject({username:"prabhanshu",
+    price:199})
+//handleObject(user);
+
+const newarray=[200,400,600,100]
+
+function getArray(thisArray){
+return thisArray[1];
+}
+
+//console.log(getArray(newarray));//200->0,1->400
+console.log(getArray([1005,40,80,64]))
